5dc9472d4d3d3193d585841a397904193faa7e96
Fabio Mortari <mortari.fabio@gmail.com>
2021-03-20 16:31:43 -0300

    Update Travis CI config

--- app/src/main/java/com/mortarifabio/marvelcharacterschallenge/utils/ApiKeys.kt
+++ app/src/main/java/com/mortarifabio/marvelcharacterschallenge/utils/ApiKeys.kt
@@ -1,8 +1,6 @@
 package com.mortarifabio.marvelcharacterschallenge.utils
 
 object ApiKeys {
-//    const val API_PUBLIC_KEY = "6eb7e8896ec5850c52515a8a23ee97f0"
-//    const val API_PRIVATE_KEY = "0dd0c16fedb8a02985977eafca66b49f5e6a526f"
     const val API_PUBLIC_KEY = "c028277ac0571a46757971ab378c6d75"
     const val API_PRIVATE_KEY = "841dc07898d43796e0514058e6064e37751dc761"
 }
